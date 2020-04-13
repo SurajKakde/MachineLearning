e672d83f5f5e484af50830367c72950c0adaf6fe
SurajKakde <suraj.lats@gmail.com>
2020-04-13 18:58:40 +0530

    testing script add

---
+++ Part1-Datapreprocessing/datapreprocessing.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 18:42:28 2020
+
+@author: Suraj
+"""
+#import libraries
+import numpy as ny
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+# importing dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x = onehotencoder.fit_transform(x).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
